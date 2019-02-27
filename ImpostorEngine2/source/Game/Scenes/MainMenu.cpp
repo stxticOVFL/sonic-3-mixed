a14485410989ff2e9a69b685a4dc4a36b60acc1d
unknown <theclashingfritz@gmail.com>
2019-02-27 18:57:16 -0500

    Fix error

--- ImpostorEngine2/source/Game/Scenes/MainMenu.cpp
+++ ImpostorEngine2/source/Game/Scenes/MainMenu.cpp
@@ -5,6 +5,7 @@
 class Scene_MainMenu : public IScene {
 public:
 	int selected = 0;
+	int ran = 0;
 	ISprite* MenuSprite = NULL;
 	ISprite* SphereSprite = NULL;
 	ISprite* SuperButtonsSprite = NULL;
